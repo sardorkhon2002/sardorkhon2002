c956341f641465dde2d3a6c1af8c66837e16eef0
Sardorkhon <t.sardor1706@gmail.com>
2024-01-16 18:47:18 +0500

    feat: comment form submit logic

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,37 +31,46 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "JavaScript Mastery",
-          from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+    alert("Thank you. I will get back to you as soon as possible.");
+    setLoading(false);
+    setForm({
+        name: "",
+        email: "",
+        message: "",
+        });
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
-        },
-      );
+    // emailjs
+    //   .send(
+    //     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+    //     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    //     {
+    //       from_name: form.name,
+    //       to_name: "JavaScript Mastery",
+    //       from_email: form.email,
+    //       to_email: "sujata@jsmastery.pro",
+    //       message: form.message,
+    //     },
+    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+    //   )
+    //   .then(
+    //     () => {
+    //       setLoading(false);
+    //       alert("Thank you. I will get back to you as soon as possible.");
+    //
+    //       setForm({
+    //         name: "",
+    //         email: "",
+    //         message: "",
+    //       });
+    //     },
+    //     (error) => {
+    //       setLoading(false);
+    //       console.error(error);
+    //
+    //       alert("Ahh, something went wrong. Please try again.");
+    //     },
+    //   );
   };
 
   return (
